3c6f360d1e6e1001cf96a05bf7761059fa752366
Kubaczu <cubic100@gmail.com>
2019-07-11 18:54:06 +0200

    local database

--- src/main/java/com/project/timesheet/User/UserRepository.java
+++ src/main/java/com/project/timesheet/User/UserRepository.java
@@ -9,7 +9,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE upper(u.lastName) LIKE concat('%',upper(?1),'%')")
     List<User> findAllByNameLike(String searchText);
 
-    @Query("UPDATE u FROM User u WHERE upper(u.lastName) LIKE concat('%',upper(?1),'%')")
-    // @Query("UPDATE usera FROM tabelaUserów WHERE u.id LIKE (?1) wartościami u.firstName=(?2), u.lastName=(?3), u.rate(?4))
-    void editUserFromInterface(int id, String firstName, String lastName, int rate);
+//    @Query("UPDATE u FROM User u WHERE upper(u.lastName) LIKE concat('%',upper(?1),'%')")
+//    @Query("UPDATE usera FROM tabelaUserów WHERE u.id LIKE (?1) wartościami u.firstName=(?2), u.lastName=(?3), u.rate(?4))
+//    void editUserFromInterface(int id, String firstName, String lastName, int rate);
 }
